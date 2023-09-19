962105644599a0023b15e992104ba84cb5b9daf2
SHREE HARI B <107598445+shreeharib@users.noreply.github.com>
2023-09-19 10:16:42 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,classification_report
 import pandas as pd
 import numpy as np
 import joblib
