e3d52e6b2f37c2b7c7f75510f6f126928a4d5965
SHREE HARI B <107598445+shreeharib@users.noreply.github.com>
2023-09-19 10:18:06 +0530

    Update train.py

--- train.py
+++ train.py
@@ -21,6 +21,6 @@ joblib.dump(model,model_path)
 
 with open("metrics.txt","w") as fw:
   fw.write(f"\nAccuracy: {accuracy_score(y_test,model.predict(X_test))}")
-  fw.write(f"\n{classification_report(y_test,clf.predict(X_test))}")
+  fw.write(f"\n{classification_report(y_test,model.predict(X_test))}")
 
 print("Training Completed")
